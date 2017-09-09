d3af115fa9a61d6f0a9f18492ffb317d76a3bb13
talilaRIX <30466531+talilaRIX@users.noreply.github.com>
2017-09-09 17:55:08 +0300

    Все исправила. Кроме одной штуки (новой). Подключила js

---
+++ js/popup.js
@@ -0,0 +1,22 @@
+var link = document.querySelector(".contacts-button");
+var popup = document.querySelector(".feedback-wrapper");
+var close = popup.querySelector(".modal-close");
+
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("feedback-show");
+});
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("feedback-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+ if (evt.keyCode === 27) {
+	if (popup.classList.contains("modal-show")) {
+	popup.classList.remove("feedback-show");
+	}
+ }
+ });
